5c93abbccccf6461b8c5a16fb069bef416ec34a0
dev-embedthis <mob-pub-18@emobrien.com>
2025-01-01 20:09:38 +1100

    DEV: add table test

---
+++ test/table.ts
@@ -0,0 +1,26 @@
+/*
+    table.ts - Test get metric table of dimensions
+ */
+import {client, table, CustomMetrics, log, dump} from './utils/init'
+
+jest.setTimeout(7200 * 1000)
+
+test('Test get metric table', async () => {
+    let metrics = new CustomMetrics({client, table, log: true})
+    let timestamp = new Date(2000, 0, 1).getTime()
+
+    let metric = await metrics.emit('test/table', 'Temp', 10, [], {timestamp})
+    metric = await metrics.emit('test/table', 'Temp', 10, [{}, {Rocket:'SaturnV'}], {timestamp})
+    metric = await metrics.emit('test/table', 'Temp', 10, [{}, {Rocket:'Falcon9'}], {timestamp})
+
+    let list = await metrics.queryMetrics('test/table', 'Temp', 300, 'sum', {timestamp})
+    expect(list).toBeDefined()
+    expect(list.length).toBe(3)
+    expect(list[0].dimensions).toEqual({})
+    expect(list[0].points.length).toBe(1)
+    expect(list[0].points[0].value).toBe(30)
+
+    expect(list[1].dimensions).toEqual({Rocket: 'Falcon9'})
+    expect(list[1].points.length).toBe(1)
+    expect(list[1].points[0].value).toBe(10)
+})
