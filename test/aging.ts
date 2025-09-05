004f5d9eb0541361a1e819d24c79f0ad64e51a1f
dev-embedthis <mob-pub-18@emobrien.com>
2025-09-05 10:19:13 +1000

    FIX: for buffered metrics to load from DynamoDB on return value from emit() and to update all spans for current timestamp

---
+++ test/aging.ts
@@ -0,0 +1,110 @@
+/*
+    aging.ts - Test aging spans in return values from emit
+
+    Edit your test case here and invoke via: "jest debug"
+
+    Or run VS Code in the top level directory and just run.
+ */
+import {client, table, CustomMetrics, log, dumpMetric, dumpQuery} from './utils/init'
+
+// jest.setTimeout(7200 * 1000)
+
+test('Test unbuffered', async () => {
+    let metrics = new CustomMetrics({client, table})
+
+    let timestamp = new Date(2000, 0, 1).getTime()
+    let metric = await metrics.emit('test/debug', 'Unbuffered', 10, [], {timestamp})
+    expect(metric.spans[0].points.length).toBe(1)
+    expect(metric.spans[0].points[0].count).toBe(1)
+    expect(metric.spans[0].points[0].sum).toBe(10)
+
+    timestamp += 30 * 1000
+    metric = await metrics.emit('test/debug', 'Unbuffered', 10, [], {timestamp})
+    expect(metric.spans[0].points.length).toBe(2)
+    expect(metric.spans[0].points[0].count).toBe(1)
+    expect(metric.spans[0].points[0].sum).toBe(10)
+    expect(metric.spans[0].points[1].sum).toBe(10)
+
+    /*
+        Test that the data points are returned
+     */
+    timestamp += 30 * 1000
+    let r = await metrics.query('test/debug', 'Unbuffered', {}, 86400, 'sum', {timestamp})
+    expect(r.points.length).toBe(12)
+    expect(r.points[11].value).toBe(20)
+    expect(r.points[11].count).toBe(2)
+
+    /*
+        Emit another value to test the prior data points are returned
+     */
+    timestamp += 12 * 3600 * 1000
+    metric = await metrics.emit('test/debug', 'Unbuffered', 100, [], {timestamp})
+    expect(metric.spans[0].points.length).toBe(1)
+    expect(metric.spans[0].points[0].count).toBe(1)
+    expect(metric.spans[0].points[0].sum).toBe(100)
+    expect(metric.spans[2].points.length).toBe(7)
+    expect(metric.spans[2].points[0].count).toBe(2)
+    expect(metric.spans[2].points[0].sum).toBe(20)
+
+    /*
+        Query all results
+     */
+    r = await metrics.query('test/debug', 'Unbuffered', {}, 86400, 'sum', {timestamp})
+    expect(r.points.length).toBe(12)
+    expect(r.points[11].value).toBe(100)
+    expect(r.points[11].count).toBe(1)
+})
+
+
+test('Test buffered', async () => {
+    let metrics = new CustomMetrics({client, table})
+
+    /*
+        Emit two buffered data points
+     */
+    let timestamp = new Date(2000, 0, 1).getTime()
+    let metric = await metrics.emit('test/debug', 'Buffered', 10, [], {timestamp, buffer: {count: 10}})
+    expect(metric.spans[0].points.length).toBe(1)
+    expect(metric.spans[0].points[0].count).toBe(1)
+    expect(metric.spans[0].points[0].sum).toBe(10)
+
+    timestamp += 60 * 1000
+    metric = await metrics.emit('test/debug', 'Buffered', 10, [], {timestamp, buffer: {count: 10}})
+    expect(metric.spans[0].points.length).toBe(2)
+    expect(metric.spans[0].points[0].count).toBe(1)
+    expect(metric.spans[0].points[0].sum).toBe(10)
+    expect(metric.spans[0].points[1].count).toBe(1)
+    expect(metric.spans[0].points[1].sum).toBe(10)
+
+    /*
+        Test that the buffered data points are returned
+     */
+    timestamp += 60 * 1000
+    let r = await metrics.query('test/debug', 'Buffered', {}, 86400, 'sum', {timestamp})
+    expect(r.points.length).toBe(12)
+    expect(r.points[11].value).toBe(20)
+    expect(r.points[11].count).toBe(2)
+
+    // Flush to persist
+    await metrics.flush()
+
+    /*
+        Do with a new instance to force re-reading from storage
+        Emit another buffered value to test the prior data points are returned
+     */
+    metrics = new CustomMetrics({client, table})
+    timestamp += 12 * 3600 * 1000
+    metric = await metrics.emit('test/debug', 'Buffered', 100, [], {timestamp, buffer: {count: 10}})
+    expect(metric.spans[0].points.length).toBe(1)
+    expect(metric.spans[0].points[0].count).toBe(1)
+    expect(metric.spans[0].points[0].sum).toBe(100)
+    expect(metric.spans[2].points.length).toBe(7)
+
+    /*
+        Query all results
+     */
+    r = await metrics.query('test/debug', 'Buffered', {}, 86400, 'sum', {timestamp})
+    expect(r.points.length).toBe(12)
+    expect(r.points[11].value).toBe(100)
+    expect(r.points[11].count).toBe(1)
+})
